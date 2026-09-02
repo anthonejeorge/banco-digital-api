c9a46c57cbe4e1daeee4fa42932457d22324e403
Anthone Jeorge <anthonejeorge@gmail.com>
2026-09-01 21:39:50 -0300

    feat: migrations adicionadas para criação de tabela e contas existentes

---
+++ src/main/resources/db/migration/V1__cria_e_popula_tabela_contas.sql
@@ -0,0 +1,14 @@
+-- Create the table
+CREATE TABLE contas (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    saldo NUMERIC(15, 2) NOT NULL DEFAULT 0.00
+);
+
+-- Insert initial data
+INSERT INTO contas (nome, saldo) VALUES ('Joe Silva', 500.00);
+INSERT INTO contas (nome, saldo) VALUES ('Karl Souza', 450.50);
+INSERT INTO contas (nome, saldo) VALUES ('John Santos', 50.50);
+INSERT INTO contas (nome, saldo) VALUES ('Maria Oliveira', 4500.00);
+INSERT INTO contas (nome, saldo) VALUES ('Darcy Ribeiro', 4.00);
+INSERT INTO contas (nome, saldo) VALUES ('Mc Maya, 48000.00);
